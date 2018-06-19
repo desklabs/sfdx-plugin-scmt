ce5a530baf907c7d23cf9047e85e58cd5a82efc1
Thomas Stachl <thomas@stachl.me>
2018-06-19 13:35:05 -0700

    clean up CommandBase
    Removed unnecessary enum `PropertyType`
    Fixed window size to viewport
    Sped up slowMo mode
    Connected browser console to process

--- src/command_base.ts
+++ src/command_base.ts
@@ -2,17 +2,9 @@ import * as Config from '@oclif/config';
 import { SfdxCommand } from '@salesforce/command';
 import { launch, Browser, Page } from 'puppeteer';
 
-export enum PropertyType {
-  TEXT = "text",
-  TEXTAREA = "textarea",
-  CHECKBOX = "checkbox",
-  BUTTON = "button",
-}
-
 export default abstract class CommandBase extends SfdxCommand {
   protected browser: Browser;
   protected page: Page;
-
 
   protected async assignOrg(): Promise<void> {
     await super.assignOrg();
@@ -21,12 +13,14 @@ export default abstract class CommandBase extends SfdxCommand {
     let debug = (this.logger.getLevel() <= 20);
 
     this.browser = await launch({
-      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+      args: ["--no-sandbox", "--disable-setuid-sandbox", '--window-size=1920,1195'],
       headless: !debug,
-      slowMo: debug ? 1000 : 0
+      slowMo: debug ? 250 : 0
     });
 
     this.page = await this.browser.newPage();
+    if (debug) this.page.on('console', console.log)
+
     await this.page.setViewport({ width: 1920, height: 1080 });
     await this.page.goto(this.buildUrl(`/secur/frontdoor.jsp?sid=${this.org.getConnection().accessToken}`));
     await this.page.waitForNavigation();
